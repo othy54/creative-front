67b8f6ca3adafdab166fd1125baa441a7e7abae8
BENSAOULA Othman <o.bensaoula@nancomcy.fr>
2024-12-19 23:27:56 +0100

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      colors : {
+        mocha: {
+          100: '#f5f0ed',
+          200: '#e0d1c9',
+          300: '#cbb2a6',
+          400: '#b69382',
+          500: '#a1745e',
+          600: '#7d5a49',
+          700: '#594034',
+          800: '#36271f',
+          900: '#120d0a',
+        }
+      } 
+    },
   },
   plugins: [],
 }
