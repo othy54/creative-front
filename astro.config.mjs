1ee0c1ec9df20b3eda201f3edba98bad4baeff02
BENSAOULA Othman <o.bensaoula@nancomcy.fr>
2024-12-21 00:42:21 +0100

    update with prettier

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,38 +1,37 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import storyblok from '@storyblok/astro';
-import { loadEnv } from 'vite';
-import tailwind from '@astrojs/tailwind'
-import basicSsl from '@vitejs/plugin-basic-ssl'
+import { defineConfig } from "astro/config";
+import storyblok from "@storyblok/astro";
+import { loadEnv } from "vite";
+import tailwind from "@astrojs/tailwind";
+import basicSsl from "@vitejs/plugin-basic-ssl";
 
 // @ts-ignore
-const env = loadEnv("", process.cwd(), 'STORYBLOK');
+const env = loadEnv("", process.cwd(), "STORYBLOK");
 
 // https://astro.build/config
 export default defineConfig({
   experimental: {
-      responsiveImages: true,
-      svg: true,
+    responsiveImages: true,
+    svg: true,
   },
 
   integrations: [
-      storyblok({
-          accessToken: env.STORYBLOK_TOKEN,
-          components: {
-              article: 'storyblok/Article',
-              content: 'storyblok/Content',
-              paragraph: 'storyblok/Paragraph',
-              titleTwo: 'storyblok/TitleTwo',
-          },
-      }),
-      tailwind(),
+    storyblok({
+      accessToken: env.STORYBLOK_TOKEN,
+      components: {
+        article: "storyblok/Article",
+        content: "storyblok/Content",
+        paragraph: "storyblok/Paragraph",
+        titleTwo: "storyblok/TitleTwo",
+      },
+    }),
+    tailwind(),
   ],
 
   vite: {
-      plugins:[basicSsl()],
-      server: {
-          https: true,
-        },
+    plugins: [basicSsl()],
+    server: {
+      https: true,
+    },
   },
-
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
-      colors : {
+      colors: {
         mocha: {
-          100: '#f5f0ed',
-          200: '#e0d1c9',
-          300: '#cbb2a6',
-          400: '#b69382',
-          500: '#a1745e',
-          600: '#7d5a49',
-          700: '#594034',
-          800: '#36271f',
-          900: '#120d0a',
-        }
-      } 
+          100: "#f5f0ed",
+          200: "#e0d1c9",
+          300: "#cbb2a6",
+          400: "#b69382",
+          500: "#a1745e",
+          600: "#7d5a49",
+          700: "#594034",
+          800: "#36271f",
+          900: "#120d0a",
+        },
+      },
     },
   },
   plugins: [],
-}
-
+};
