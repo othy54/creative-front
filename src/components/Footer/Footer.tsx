15af05c139c02f58f9f0f4b442d306ed8f03da72
othy54 <othman.bensaoula@gmail.com>
2025-01-05 23:38:46 +0100

    first commit dark mode

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,8 +4,8 @@ import { motion, useScroll, useMotionTemplate, useTransform } from "motion/react
 import { useRef } from 'react';
 
 
-export default function Footer() {
-    const footer = useRef(null);
+const Footer = () => {
+    const footer = useRef<HTMLElement | null>(null);
 
     const { scrollYProgress } = useScroll({
         target: footer,
@@ -15,25 +15,27 @@ export default function Footer() {
     const blurValue = useTransform(scrollYProgress, [0, 1], [100, 0]);
     const filter = useMotionTemplate`blur(${blurValue}px)`;
 
-    const year = new Date();
+    const year: number = new Date().getFullYear();
 
     return (
-        <footer ref={footer} className="bg-mocha-500 mt-32 py-5 lg:py-10">
+        <footer ref={footer} className="bg-mocha-200 dark:bg-mocha-800 mt-32 py-5 lg:py-10">
             <div
                 className={styles.container + " max-w-[1280px] min-h-[40vh] mx-auto pt-4 lg:pt-10 px-6 lg:px-8 flex justify-center items-center"}
             >
                 <div className={styles.wrapper}>
-                    <motion.div className={styles.logo} style={{ filter }}>
+                    <motion.div className={styles.logo + ' text-mocha-900 dark:text-mocha-100'} style={{ filter }}>
                         CREATIVE-FRONT
                     </motion.div>
                 </div>
             </div>
             <div className="max-w-[1280px] mx-auto">
-                <hr className="border-mocha-600" />
-                <div className="mt-2 text-right">
-                    © {year.getFullYear()} CREATIVE-FRONT
+                <hr className="border-mocha-300" />
+                <div className="mt-2 text-right text-sm font-semibold text-mocha-900 dark:text-mocha-100">
+                    © {year} CREATIVE-FRONT
                 </div>
             </div>
         </footer>
     )
 }
+
+export default Footer;
